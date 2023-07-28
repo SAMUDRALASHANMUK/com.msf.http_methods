396a9d2ba7a00d9e52f13db8e6b48e58f3c1b1e0
SAMUDRALASHANMUK <samudralashanmuk2017@gmail.com>
2023-07-28 17:01:06 +0530

    ktor

--- src/main/kotlin/com/msf/routes/DownloadRoutes.kt
+++ src/main/kotlin/com/msf/routes/DownloadRoutes.kt
@@ -5,21 +5,3 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
-
-=
-
-
-fun Application.configureDownloadRoutes() {
-    routing {
-        get("/fileDownload") {
-            val file = File("files/americano.jpg")
-            call.response.header(
-                HttpHeaders.ContentDisposition,
-                ContentDisposition.Attachment.withParameter(
-                    ContentDisposition.,
-                    "downloadableimage.jpg"
-                ).toString()
-            )
-        }
-    }
-}
